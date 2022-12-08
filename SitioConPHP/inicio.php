767f700fe2eefd34784098419f127714d6af1cd9
PC ESCRITORIO <fernando95f4@gmail.com>
2022-12-08 14:29:13 -0500

    Primer commit usando repositorio para Dreamweaver y mas.

---
+++ SitioConPHP/inicio.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<?php
+	phpinfo();
+?>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+	<h1>Hola PHP, desde otra ruta para GitHub</h1>
+<body>
+</body>
+</html>
