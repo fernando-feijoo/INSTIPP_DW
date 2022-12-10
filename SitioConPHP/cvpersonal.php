6c897be95fabb5dc477f44615afe0695d1797a53
PC PORTATIL <fernando95f4@gmail.com>
2022-12-09 21:01:01 -0500

    Cambios nuevos en el instituto

---
+++ SitioConPHP/cvpersonal.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>HV Fernando F. Feijoo</title>
+</head>
+<body>
+	<?php
+	$_txtnombre = "Fernando F. Feijoo";
+	$_txtdescripcion = "Desarrollador web, movil y escritorio... Me adapto con facilida a equipos de trabajo y contribuyo en los proyectos con 101% de responsabilidad y empeño.";
+		
+	echo "<h2>$_txtnombre</h2>
+    <img src=\"https://cdn.discordapp.com/attachments/930868835060428820/1041488272993562685/Foto_tamano_carnet_con_fondo.jpg\" width=\"150px\"  >
+    <p>Tecnologo Superior en Desarrollo de Software</p>
+    <hr align=\"left\" width=40% color=\"black\"/>
+    <h2><i>Resumen</i></h2>
+    <p>$_txtdescripcion</p>
+    <hr  align=\"left\" width=25% size=\"5\" color=\"black\"/>
+    <h2><i>Experiencia</i></h2>
+    <p><b>Co-Fundador y Presidente</b> | 2021 - Actualidad <br><sub>Delimit-Ingenieria S.A.S</sub></p>
+    <hr align=\"left\" width=\"25%\" size=\"1\" color=\"black\">
+    <p><b>Cooperativa de Ahorro y Credito \"Santa Rosa\" Ltda.</b> | 2017 - 2019</p>
+    <ul>
+        <li><b>Asistente de Operaciones - Junior</b><br><sub>Sep. 2019 - Oct 2019</sub></li>
+        <hr align=\"left\" width=\"25%\" size=\"1\" color=\"black\">
+        <li><b>Asistente de Credito y Liquidador - Senior</b><br><sub>Jun. 2018 - Ago. 2019</sub></li>
+        <hr align=\"left\" width=\"25%\" size=\"1\" color=\"black\">
+        <li><b>Recibidor/Pagador (Caja) - Senior</b><br><sub>Oct. 2017 - May. 2018</sub></li>
+    </ul>
+    <hr  align=\"left\" width=\"25%\" size=\"5\" color=\"black\"/>
+    <h2><i>Estudios</i></h2>
+    <ul>
+        <li><b>Instituto Superior Tecnologico Ismael Perez Pazmiño</b><br><sub><i>Tecnologo Superior en Desarrollo de Software</i><br>Dic. 2019 - ± Ene 2024</sub></li>
+        <hr align=\"left\" width=\"25%\" size=\"1\" color=\"black\">
+        <li><b>Colegio de Bachillerato Carmen Mora de Encalada</b><br><sub><i>Bachiller Fisico Matematico</i><br>2007 - 2013</sub></li>
+    </ul>
+    <hr  align=\"left\" width=\"25%\" size=\"5\" color=\"black\"/>
+    <h2><i>Tecnologias</i></h2>
+</body>
+<footer>
+    <p align=\"center\"><sub>CopyRight © <b>$_txtnombre 👨‍💻 </b></sub></p>
+</footer>";
+	?>
+</body>
+</html>
---
+++ SitioConPHP/prueba.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Pruebas con PHP</title>
+</head>
+
+<body>
+<?php
+echo '<h1>Hola PHP, vamos a ver que tal.</h1>
+		<p>Esta es mi primera vez en PHP.</p>';
+	$autor = "Bryan Carmona";
+	echo "<h2>Mi segunda vez en PHP</h1>
+	<p>Esta es la segunda vez que hago una pagina como: $autor.</p>";
+	$autor = "Fernando F. Feijoo";
+	echo "<p>Con soporte de: $autor.</p>";
+?>
+</body>
+</html>
